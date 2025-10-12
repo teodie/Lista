a287b51503303f685a87e9f44cf498e1e465e8da
teodie <54758584+teodie@users.noreply.github.com>
2025-10-13 00:40:49 +0800

    Commiting before cleaning the prebuild for the installation of react native fs and whisper.rn

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -11,4 +11,8 @@
 -keep class com.swmansion.reanimated.** { *; }
 -keep class com.facebook.react.turbomodule.** { *; }
 
+# whisper.rn
+-keep class com.rnwhisper.** { *; }
+
 # Add any project specific keep options here:
+
--- app/(app)/record.jsx
+++ app/(app)/record.jsx
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput } from 'react-native'
+import React, { useState, useRef, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const record = () => {
+  const [text, setText] = useState('')
+  const inputRef = useRef(null)
+
+  useEffect(()=>{
+    inputRef.current.focus()
+  }, [])
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{alignItems: 'center'}}>
       <Text>record</Text>
+      <TextInput 
+        ref={inputRef}
+        keyboardType='default'
+        value={text}
+        onChangeText={setText}
+        style={{display: 'flex', borderWidth: 1}}
+      />
     </SafeAreaView>
   )
 }
