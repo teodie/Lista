6424bc49ebbed4b285b8c056d994e575600763c6
teodie <54758584+teodie@users.noreply.github.com>
2025-10-14 17:29:11 +0800

    feat: added feature for requesting permissioin for audio recording

--- app/(app)/record.jsx
+++ app/(app)/record.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ActivityIndicator, Button, Text } from 'react-native-paper'
 import { initWhisper } from 'whisper.rn'
-import { View } from 'react-native'
+import { PermissionsAndroid, View, Alert, Linking } from 'react-native'
 
 const record = () => {
   const [text, setText] = useState('')
@@ -28,6 +28,66 @@ const record = () => {
     setTranscribing(false)
   }
 
+  const changePermissionInSettings = () => {
+    Alert.alert(
+      'Audio Recording Permission',
+      'Persmision is needed to use voice typing feature of the app.',
+      [
+        { text: 'Dismis', style: 'cancel' },
+        {
+          text: 'Open Settings',
+          style: 'default',
+          onPress: () => {
+            Linking.openSettings()
+          }
+        },
+      ]
+
+    )
+  }
+
+  const requestAudioPermission = async () => {
+    const audioRecordingPermitted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO)
+
+    console.log("Current Permission: ", audioRecordingPermitted)
+    
+    if(audioRecordingPermitted) return console.log("Permission already granted")
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: 'Recording Permission',
+          message:
+            'Lista App needs access to your audio recording ' +
+            'so you can use voice typing feature of the app.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      )
+
+      if (granted === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+        changePermissionInSettings()
+      }
+
+      return granted
+
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+
+  }
+
+  const startRecord = async () => {
+    
+    
+    const result = await requestAudioPermission()
+
+
+  }
+
   return (
     <SafeAreaView style={{ alignItems: 'center', justifyContent: 'center', flex: 1, borderWidth: 1, gap: 20 }}>
 
@@ -37,10 +97,17 @@ const record = () => {
           : <Text>{text}</Text>
       }
 
-      <Button
-        mode='contained'
-        onPress={transcribe}
-      >Transcribe</Button>
+      <View style={{ flexDirection: 'row', gap: 20 }}>
+        <Button
+          mode='contained'
+          onPress={startRecord}
+        >Record</Button>
+
+        <Button
+          mode='contained'
+          onPress={transcribe}
+        >Transcribe</Button>
+      </View>
     </SafeAreaView>
   )
 }
