65257935771c2cb7dab10276c47c5b86783ff3c6
teodie <54758584+teodie@users.noreply.github.com>
2025-10-15 16:37:35 +0800

    style: Combining the record and stop button into one button

--- app/(app)/record.jsx
+++ app/(app)/record.jsx
@@ -5,11 +5,39 @@ import { initWhisper } from 'whisper.rn'
 import { PermissionsAndroid, View, Alert, Linking } from 'react-native'
 import { useAudioRecorder, RecordingPresets } from 'expo-audio'
 
+
 const record = () => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState('Sample text')
   const [transcribing, setTranscribing] = useState(false)
   const [recordingPath, setRecordingPath] = useState(null)
   const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY)
+  const [onGoingRecording, setOnGoingRecording] = useState(false)
+
+
+  const realTimeTranscriber = async () => {
+    const modelPath = require('@/assets/models/ggml-tiny.bin')
+
+    const whisperContext = await initWhisper({
+      filePath: modelPath,
+    })
+
+    const options = { language: null }
+
+    const { stop, subscribe } = await whisperContext.transcribeRealtime(options)
+
+    subscribe((evt) => {
+      const { isCapturing, data, processTime, recordingTime } = evt
+      console.log(
+        `Realtime transcribing: ${isCapturing ? 'ON' : 'OFF'}\n` +
+        `Result: ${data.result}\n\n` +
+        `Process time: ${processTime}ms\n` +
+        `Recording time: ${recordingTime}ms`,
+      )
+      if (!isCapturing) console.log('Finished realtime transcribing')
+    })
+
+
+  }
 
   const transcribe = async () => {
     setTranscribing(true)
@@ -79,14 +107,18 @@ const record = () => {
 
   }
 
+  const convertAudioM4aToWav = () => {
+
+  }
+
   const startRecord = async () => {
+    setOnGoingRecording(true)
 
     const result = await requestAudioPermission()
 
     if (result !== PermissionsAndroid.RESULTS.GRANTED) return changePermissionInSettings()
 
     if (result === PermissionsAndroid.RESULTS.GRANTED) {
-
       await audioRecorder.prepareToRecordAsync();
       audioRecorder.record();
 
@@ -97,31 +129,37 @@ const record = () => {
     console.log(`Audio recording is available in ${audioRecorder.uri}`)
     setRecordingPath(audioRecorder.uri)
     await audioRecorder.stop();
+
+    setOnGoingRecording(false)
+  }
+
+  const handleRecordPress = async () => {
+    if (onGoingRecording) {
+      await stopRecording()
+    } else {
+      await startRecord()
+    }
   }
 
   return (
-    <SafeAreaView style={{ alignItems: 'center', justifyContent: 'flex-end', flex: 1, borderWidth: 1, gap: 20, paddingBottom: 50}}>
+    <SafeAreaView style={{ flex: 1, borderWidth: 1, gap: 20, paddingBottom: 50 }}>
 
-      {
-        transcribing
-          ? <ActivityIndicator />
-          : <Text>{text}</Text>
-      }
-
-      <View style={{ flexDirection: 'row', gap: 20 }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {
+          transcribing
+            ? <ActivityIndicator />
+            : <Text>{text}</Text>
+        }
+      </View>
+      <View style={{ flexDirection: 'row', gap: 20, justifyContent: 'center' }}>
         <Button
           mode='contained'
-          onPress={startRecord}
-        >Record</Button>
-
-        <Button
-          mode='contained'
-          onPress={stopRecording}
-        >Stop</Button>
+          onPress={handleRecordPress}
+        >{onGoingRecording ? "Stop" : "Record"}</Button>
 
         <Button
           mode='contained'
-          onPress={transcribe}
+          onPress={realTimeTranscriber}
         >Transcribe</Button>
       </View>
     </SafeAreaView>
