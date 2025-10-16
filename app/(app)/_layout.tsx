5a41e434b8280143122919b84e6c7ffe257b336c
teodie <54758584+teodie@users.noreply.github.com>
2025-10-16 22:58:07 +0800

    feat: Added back button on the items screen

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs, useRouter } from 'expo-router';
+import { Tabs, useNavigation, useRouter } from 'expo-router';
 import 'react-native-reanimated';
 import Payment from '@/components/Payment';
 import { router } from 'expo-router';
@@ -9,6 +9,7 @@ import { ClientProvider } from '@/utils/client-context';
 import { ItemsProvider } from '@/utils/items-context';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import Ionicons from '@expo/vector-icons/Ionicons';
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function RouteGaurd({ children }: { children: React.ReactNode }) {
@@ -39,81 +40,94 @@ function RouteGaurd({ children }: { children: React.ReactNode }) {
   return <>{children}</>
 }
 
+// Back icon in the header
+const BackIcon = () => (
+  <MaterialIcons
+    name="arrow-back"
+    size={30} color="white"
+    style={{ paddingHorizontal: 10 }}
+    onPress={() => router.back()}
+  />
+)
+
 export default function ScreenLayout() {
   const router = useRouter()
 
   return (
     <SafeAreaProvider>
-        <RouteGaurd>
-          <ClientProvider>
-            <ItemsProvider>
-              <Tabs screenOptions={{ tabBarActiveTintColor: '#5959B2' }}>
-                <Tabs.Screen
-                  name='index'
-                  options={{
-                    title: "Home",
-                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-                    headerShown: false,
-                  }}
-                />
+      <RouteGaurd>
+        <ClientProvider>
+          <ItemsProvider>
+            <Tabs screenOptions={{ tabBarActiveTintColor: '#5959B2' }}>
+              <Tabs.Screen
+                name='index'
+                options={{
+                  title: "Home",
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                  headerShown: false,
+                }}
+              />
 
-                <Tabs.Screen
-                  name='items'
-                  options={{
-                    headerShown: true,
-                    title: 'Details', headerRight: () => <Payment />,
-                    headerStyle: { backgroundColor: '#5959B2' },
-                    headerTintColor: 'white',
-                    href: null,
-                  }} />
+              <Tabs.Screen
+                name='items'
+                options={{
+                  headerBackButtonDisplayMode: 'default',
+                  headerShown: true,
+                  title: 'Details',
+                  headerLeft: () => <BackIcon />,
+                  headerRight: () => <Payment />,
+                  headerStyle: { backgroundColor: '#5959B2' },
+                  headerTintColor: 'white',
+                  href: null,
+                }} />
 
-                <Tabs.Screen
-                  name='analytics'
-                  options={{
-                    title: 'Analytics',
-                    tabBarIcon: ({ color }) => <MaterialIcons name="analytics" size={24} color={color} />,
-                    headerShown: false
-                  }} />
+              <Tabs.Screen
+                name='analytics'
+                options={{
+                  title: 'Analytics',
+                  tabBarIcon: ({ color }) => <MaterialIcons name="analytics" size={24} color={color} />,
+                  headerShown: false
+                }} />
 
-                <Tabs.Screen
-                  name='record'
-                  options={{
-                    title: 'Record',
-                    tabBarIcon: ({ color, focused }) =>
-                      <TouchableOpacity
-                        onPress={() => { router.navigate('/record') }}
-                        style={{ height: 70, width: 70, alignItems: 'center', justifyContent: 'center', borderRadius: 35, position: 'absolute', bottom: 0, backgroundColor: '#5959B2', overflow: 'hidden' }}
-                      >
-                        <MaterialIcons name="mic" size={40} color="white" />
-                      </TouchableOpacity>,
-                    headerShown: false,
-                  }}
-                />
+              <Tabs.Screen
+                name='record'
+                options={{
+                  title: 'Record',
+                  tabBarIcon: ({ color, focused }) =>
+                    <TouchableOpacity
+                      onPress={() => { router.navigate('/record') }}
+                      style={{ height: 70, width: 70, alignItems: 'center', justifyContent: 'center', borderRadius: 35, position: 'absolute', bottom: 0, backgroundColor: '#5959B2', overflow: 'hidden' }}
+                    >
+                      <MaterialIcons name="mic" size={40} color="white" />
+                    </TouchableOpacity>,
+                  headerShown: false,
+                }}
+              />
 
-                <Tabs.Screen
-                  name='add'
-                  options={{
-                    headerTitle: 'Create new client',
-                    tabBarIcon: ({ color }) => <Ionicons name="person-add" size={24} color={color} />,
-                    headerShown: true,
-                    headerTintColor: 'white',
-                    headerStyle: { backgroundColor: '#5959B2' },
-                    headerTitleAlign: 'center'
-                  }}
+              <Tabs.Screen
+                name='add'
+                options={{
+                  headerTitle: 'Create new client',
+                  tabBarIcon: ({ color }) => <Ionicons name="person-add" size={24} color={color} />,
+                  headerShown: true,
+                  headerTintColor: 'white',
+                  headerStyle: { backgroundColor: '#5959B2' },
+                  headerTitleAlign: 'center'
+                }}
 
-                />
+              />
 
-                <Tabs.Screen
-                  name='settings'
-                  options={{
-                    title: 'Settings',
-                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-                    headerShown: false,
-                  }} />
-              </Tabs>
-            </ItemsProvider>
-          </ClientProvider>
-        </RouteGaurd>
+              <Tabs.Screen
+                name='settings'
+                options={{
+                  title: 'Settings',
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                  headerShown: false,
+                }} />
+            </Tabs>
+          </ItemsProvider>
+        </ClientProvider>
+      </RouteGaurd>
     </SafeAreaProvider>
   );
 }
